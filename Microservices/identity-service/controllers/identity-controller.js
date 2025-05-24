32da4027ef6642a2c2da3f46c51f641e6f24a999
Jeet Sharma <jeetsharma31179@gmail.com>
2025-05-25 00:16:47 +0530

    Identity service - register user done

---
+++ Microservices/identity-service/controllers/identity-controller.js
@@ -0,0 +1,56 @@
+const logger = require('../utils/logger');
+const {validateUserRegistration} = require('../utils/validator');
+const {generateToken} = require('../utils/generateToken');
+const User = require('../models/User');
+
+//user registration
+const registerUser = async (req, res) => {
+    try {
+
+        //validate the request body -- through validator 
+        const { error } = validateUserRegistration(req.body);
+        if (error) {
+            logger.warn('Validation error during user registration', { error: error.details[0].message });
+            return res.status(400).json({ success : false, message: error.details[0].message });
+        }
+
+        const { username, email, password } = req.body;
+        // Check if user already exists
+        let user = await User.findOne({ $or: [{ username }, { email }] });
+        if (user) {
+            logger.warn('User already exists', { username, email });
+            return res.status(409).json({ success: false, message: 'User already exists' });
+        }
+
+        // Create a new user
+        user = new User  ({ username, email, password });
+        await user.save();
+        logger.info('User registered successfully', { username, email });
+
+        //generate tokens
+        const {accessToken, refreshToken} = await generateToken(user);
+
+        res.status(201).json({ success : true , message: 'User registered successfully' , accessToken, refreshToken });
+
+    } catch (error) {
+        logger.error('Error during user registration', { error });
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+}
+
+
+
+//user login
+
+
+
+
+//refresh token
+
+
+
+
+//logout
+
+
+module.exports = {registerUser}
---
+++ Microservices/identity-service/utils/validator.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const validateUserRegistration = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().min(3).max(30).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        // confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+    });
+
+    return schema.validate(data);
+};
+
+module.exports = {validateUserRegistration};
